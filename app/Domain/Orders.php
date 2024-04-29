e719a5a36e28b9fe0b5c95482098450e56d7a7a9
Nurdin <mrsunnysummer@gmail.com>
2024-04-29 23:37:02 +0700

    Create Orders domain and create OrdersRepository functionality

--- app/Domain/Orders.php
+++ app/Domain/Orders.php
@@ -5,12 +5,8 @@ class Orders
 {
     public int $id;
     public int $account_id;
-    public int $kapster_id;
-    public string $order_date ;
-    public string $password;
-    public string $name;
-    public string $phone;
-    public string $role;
-    public ?string $profile_pic;
-    public ?string $address;
+    public string $order_date;
+    public int $total_price;
+    public string $status;
+    public int $schedule_id;
 }
--- app/Repository/OrdersRepository.php
+++ app/Repository/OrdersRepository.php
@@ -1,6 +1,7 @@
 <?php
 namespace Nurdin\Djawara\Repository;
 
+use Nurdin\Djawara\Domain\Orders;
 use PDO;
 
 class OrdersRepository
@@ -12,11 +13,60 @@ class OrdersRepository
         $this->connection = $connection;
     }
 
-    // public function save(Schedules $schedules): Schedules
-    // {
-    //     $stmt = $this->connection->prepare("INSERT INTO schedules(kapster_id, category_id, start_date, end_date, status) VALUES (?, ?, ?, ?, ?)");
-    //     $stmt->execute([$schedules->kapster_id, $schedules->category_id, $schedules->start_date, $schedules->end_date, $schedules->status]);
+    public function save(Orders $orders): Orders
+    {
+        $stmt = $this->connection->prepare("INSERT INTO orders(account_id, total_price, schedule_id, status) VALUES (?, ?, ?)");
+        $stmt->execute([$orders->account_id, $orders->total_price, $orders->schedule_id]);
 
-    //     return $schedules;
-    // }
+        return $orders;
+    }
+
+    public function findById(string $id) : ?Orders
+    {
+        $stmt = $this->connection->prepare("SELECT * FROM orders WHERE id = ?");
+        $stmt->execute([$id]);
+
+        if($row = $stmt->fetch()){
+            $orders = new Orders();
+            $orders->id = $row['id'];
+            $orders->account_id = $row['account_id'];
+            $orders->order_date = $row['order_date'];
+            $orders->total_price = $row['total_price'];
+            $orders->status = $row['status'];
+            $orders->schedule_id = $row['schedule_id'];
+
+            return $orders;
+        } else {
+            return null;
+        }
+    }
+
+    public function findAll() : ?array
+    {
+        $stmt = $this->connection->prepare('SELECT * FROM getallorders ORDER BY order_date ASC');
+        $stmt->execute();
+
+        if($stmt->rowCount() > 0){
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        } else {
+            return null;
+        }
+        
+    }
+
+    public function update(Orders $orders) : Orders
+    {
+        $stmt = $this->connection->prepare("UPDATE orders SET account_id = ?, total_price = ?, status = ?, schedule_id = ?, WHERE id = ?");
+        $stmt->execute([$orders->account_id, $orders->total_price, $orders->status, $orders->schedule_id, $orders->id]);
+
+        return $orders;
+    }
+
+    public function remove(string $id) 
+    {
+        $stmt = $this->connection->prepare("DELETE FROM orders WHERE id = ?");
+        $stmt->execute([$id]);
+    }
+
+
 }
