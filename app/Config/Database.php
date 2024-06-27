b28c908344b6b48cd633493281913bfaa9774de9
Nurdin <mrsunnysummer@gmail.com>
2024-06-28 06:23:45 +0700

    Update database password to use env

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -1,33 +1,42 @@
 <?php
+
 namespace Nurdin\Djawara\Config;
+
+use Dotenv\Dotenv;
 use PDO;
-class Database{
+
+class Database
+{
     private static ?PDO $connection = null;
 
-    public static function getConnect() : PDO
+    public static function getConnect(): PDO
     {
+        $dotenv = Dotenv::createImmutable(__DIR__ . "/../../");
+        $dotenv->load();
         if (self::$connection == null) {
             $host = "localhost";
             $port = 3306;
             $dbName = "djawara_barbershop";
             $username = "root";
-            $password = "hishasyl115a1408";
-            
+            $password = $_ENV["DB_PASS"];
+
             self::$connection = new PDO("mysql:host=$host:$port;dbname=$dbName", $username, $password);
-        } 
+        }
         return self::$connection;
     }
 
-    public static function beginTransaction(){
+    public static function beginTransaction()
+    {
         self::$connection->beginTransaction();
     }
 
-    public static function commitTransaction(){
+    public static function commitTransaction()
+    {
         self::$connection->commit();
     }
 
-    public static function rollbackTransaction(){
+    public static function rollbackTransaction()
+    {
         self::$connection->rollBack();
     }
-
 }
